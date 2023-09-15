d2fa3aedcbb7a635e8a3f0453facc33314c4bd03
khaled39ab <khaled39ab@gmail.com>
2023-09-15 21:36:13 +0600

    create new middleware verifyAdmin and successfully work

--- index.js
+++ index.js
@@ -56,6 +56,20 @@ async function run() {
         const doctorsCollection = client.db("doctors-portal").collection("Doctors");
 
 
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester })
+
+            if (requesterAccount.role === 'admin') {
+                next()
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' })
+            }
+        }
+
+
         /* ========================================================================================================== */
         app.get('/availableAppointment', async (req, res) => {
             const date = req.query.date;
@@ -150,24 +164,21 @@ async function run() {
 
 
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
 
-            const requester = req.decoded.email;
-            const requesterAccount = await usersCollection.findOne({ email: requester })
+            // const requester = req.decoded.email;
+            // const requesterAccount = await usersCollection.findOne({ email: requester })
 
-            if (requesterAccount.role === 'admin') {
+            // if (requesterAccount.role === 'admin') {
                 const updateDoc = {
                     $set: { role: 'admin' }
                 };
 
                 const result = await usersCollection.updateOne(filter, updateDoc);
                 res.send(result);
-            }
-            else {
-                res.status(403).send({ message: 'forbidden' })
-            }
+            // }
 
         });
 
